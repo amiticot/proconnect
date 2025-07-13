fa4052950b71a7420c13f4cacf0079e7c063cffb
amiticot <icotbpl@gmail.com>
2025-07-13 15:44:13 +0530

    First Commit

---
+++ src/main/java/com/proconnect/proconnect/ProconnectApplication.java
@@ -0,0 +1,13 @@
+package com.proconnect.proconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProconnectApplication.class, args);
+	}
+
+}
